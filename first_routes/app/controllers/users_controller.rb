b68af30ede9b7b9b7d7d3edd600cf4b0f1c8466f
hojung <hoj0210@gmail.com>
2020-12-03 12:39:25 -0800

    Finished first_routes project

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def index 
+        render json: User.all
+    end
+
+    def create 
+        user = User.new(params.require(:user).permit(:id, :name, :email))
+        #replace the `user_attributes_here` with the actual attribute keys
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show 
+        render json: params
+    end
 end
